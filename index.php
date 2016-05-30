6c1c803bae7900b2cd5435f290e89947283f0af5
mickwan <mickael.rinner@gmail.com>
2016-05-30 15:07:55 +0200

    page profil admin

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		require ("models/".$className.".class.php");
 	}
 
-	$access = array('logout', 'login', 'register', 'home', 'shop', 'product', 'cart', 'profile', 'cart_user', 'edit_contact', 'feedback', 'cat_admin', 'product_admin', 'cart_admin', 'test');
+	$access = array('logout', 'login', 'register', 'home', 'shop', 'product', 'current_cart', 'profile', 'cart', 'edit_contact', 'feedback', 'cat_admin', 'product_admin');
 	$page = 'home'; /*page courante : home par default*/ 
 	$error = '';
 
@@ -32,9 +32,8 @@
 								"logout" => "user", 
 								"register" => "user", 
 								"edit_contact" => "user",
-								"cart" => "cart", 
-								"cart_user" => "cart", 
-								"cart_admin" => "cart", 
+								"current_cart" => "cart", 
+								"cart" => "cart",  
 								"feedback" => "feedback", 
 								"cat_admin" => "cat_admin", 
 								"product_admin" => "product_admin"
