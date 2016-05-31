fd3d0c2e69f98a4adf336d0951a58447656ce30a
mickwan <mickael.rinner@gmail.com>
2016-05-31 10:39:14 +0200

    plein de changement

--- apps/contents/edit_contact.php
+++ apps/contents/edit_contact.php
@@ -1,8 +1,14 @@
 <?php
+	$Mcheck = '';
+	$Fcheck = '';
 	if (isset($_SESSION['id_user']))
 	{
 		$user_manager = new UsersManager($link);
 		$user = $user_manager->findById($_SESSION['id_user']);
+		if ($user->getSex() == 'M')
+			$Mcheck = 'checked';
+		else
+			$Fcheck = 'checked';
 		require 'views/contents/edit_contact.phtml';
 	}
 	else 
