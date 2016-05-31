145caddacb1d89da807ba3e3b25dbfca64728d2e
mickwan <mickael.rinner@gmail.com>
2016-05-31 15:09:19 +0200

    feedback fonctionnel

--- apps/contents/add_edit_feedback.php
+++ apps/contents/add_edit_feedback.php
@@ -1,14 +1,25 @@
 <?php
 	try
 	{
-		if (isset($_GET['id'], $_GET['action']))
+		$option = "Add ";
+		$content = "Feedback content...";
+		$id_product = '';
+
+		if (isset($_GET['id'], $_GET['action']) && $_GET['action'] == 'edit')
 		{
-			$id = intval($_GET['id']);
-			$feedbackManager = new FeedbackManager($link);
-			$feedback = $feedbackManager->findById($id);
+			if ($_GET['action'] == 'edit')
+			{
+				$id = intval($_GET['id']);
+				$feedbackManager = new FeedbackManager($link);
+				$feedback = $feedbackManager->findById($id);
+
+				$option = "Edit ";
+				$content = $feedback->getContent();
+			}
+			else if ($_GET['action'] == 'add')
+				$id_product = $_GET['id_product'];
 		}
-		else 
-			throw new Exception("No id");
+		require 'views/contents/add_edit_feedback.phtml';
 	}
 	catch (Exception $exception)
 	{
--- apps/treatments/traitement_feedback.php
+++ apps/treatments/traitement_feedback.php
@@ -27,14 +27,27 @@
 				}
 				else if ($_SESSION['admin'] == 0)
 				{
-					$id = intval($_GET['id']);
 					$feedbackManager = new FeedbackManager($link);
-					$feedback = $feedbackManager->findById($id);
-					if ($_GET['action'] == 'edit')
+			
+					if (isset($_POST['action']) && $_POST['action'] == 'add')
 					{
-
+						$feedbackManager->create($_POST);
+						header("Location: index.php?page=product&id_product=".$_POST['id_product']."");
+						exit;
 					}
-					else if ($_GET['action'] == 'delete')
+
+					$id = intval($_GET['id']);
+					$feedback = $feedbackManager->findById($id);
+					if (isset($_POST['action']) && $_POST['action'] == "edit")
+					{
+						$feedback->setContent($_POST['content']);
+						$feedback->setStatus(0);
+						$feedbackManager->update($feedback);
+						header('Location: index.php?page=feedback');
+						exit;
+					}
+
+					if ($_GET['action'] == 'delete')
 					{
 						$feedbackManager->remove($feedback);
 						header('Location: index.php?page=feedback');
