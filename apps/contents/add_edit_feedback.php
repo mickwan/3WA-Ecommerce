ce8453b9286021d3fb10604f308d50bdd033ad58
mickwan <mickael.rinner@gmail.com>
2016-05-31 14:10:32 +0200

    modif

--- apps/contents/add_edit_feedback.php
+++ apps/contents/add_edit_feedback.php
@@ -1,7 +1,14 @@
 <?php
 	try
 	{
-		
+		if (isset($_GET['id'], $_GET['action']))
+		{
+			$id = intval($_GET['id']);
+			$feedbackManager = new FeedbackManager($link);
+			$feedback = $feedbackManager->findById($id);
+		}
+		else 
+			throw new Exception("No id");
 	}
 	catch (Exception $exception)
 	{
