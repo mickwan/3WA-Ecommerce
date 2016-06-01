c070f2de17ef16240a87d58ccc6b933791fce149
MarloneB <bertrandmarlone@gmail.com>
2016-06-01 15:01:36 +0200

    maj

--- config.php
+++ config.php
@@ -1,12 +1,9 @@
 <?php
 	$localhost = 'localhost';
 	$login = 'root';
-<<<<<<< HEAD
+
 	$pass = 'root';
 
 	$database = 'slippy_shop';
-=======
-	$pass = 'troiswa';
-	$database = 'campshop';
->>>>>>> 6c7f4b63f85bf6e23154236aedbe7c28f44b4a60
+
 ?>
