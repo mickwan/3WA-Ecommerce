01c72a45721351f92b52146b8d623ca9d294aa5f
Arteast <arteast.academy@gmail.com>
2016-06-09 10:31:49 +0200

    sql

--- campshop.sql
+++ campshop.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client: localhost
--- Généré le: Mer 08 Juin 2016 à 13:35
+-- Généré le: Jeu 09 Juin 2016 à 10:31
 -- Version du serveur: 5.5.47-0ubuntu0.14.04.1
 -- Version de PHP: 5.5.9-1ubuntu4.14
 
@@ -38,7 +38,14 @@ CREATE TABLE IF NOT EXISTS `address` (
   `type` varchar(15) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `address`
+--
+
+INSERT INTO `address` (`id`, `id_user`, `name`, `number`, `pathway`, `city`, `country`, `zipcode`, `type`) VALUES
+(3, 7, 'momo', 1, 'rue du taf', 'Strasbourg', 'France', 67000, '0');
 
 -- --------------------------------------------------------
 
@@ -56,14 +63,7 @@ CREATE TABLE IF NOT EXISTS `cart` (
   `weight` float NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `id_user` (`id_user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
-
---
--- Contenu de la table `cart`
---
-
-INSERT INTO `cart` (`id`, `id_user`, `date`, `status`, `price`, `nb_products`, `weight`) VALUES
-(2, 4, '2016-06-07 11:55:11', 0, 100.99, 20, 20);
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 -- --------------------------------------------------------
 
@@ -103,16 +103,17 @@ CREATE TABLE IF NOT EXISTS `feedback` (
   PRIMARY KEY (`id`),
   KEY `id_author` (`id_author`),
   KEY `id_product` (`id_product`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Contenu de la table `feedback`
 --
 
 INSERT INTO `feedback` (`id`, `id_author`, `id_product`, `content`, `date`, `status`) VALUES
-(1, 6, 1, 'voila voila', '2016-06-07 11:54:21', 0),
-(2, 4, 3, 'houlaaaaaa', '2016-06-07 11:54:21', 0),
-(3, 6, 1, 'coucou c''est moi FEEDBACK =)', '2016-06-07 12:46:21', 1);
+(1, 6, 1, 'voila voila', '2016-06-07 11:54:21', 2),
+(2, 4, 3, 'houlaaaaaa', '2016-06-07 11:54:21', 2),
+(3, 6, 1, 'coucou c''est moi FEEDBACK =)', '2016-06-07 12:46:21', 1),
+(4, 7, 1, 'je suis un commentaire', '2016-06-08 13:58:53', 0);
 
 -- --------------------------------------------------------
 
@@ -227,7 +228,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `admin` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0:user / 1:admin',
   `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '0:inactif / 1:actif',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 --
 -- Contenu de la table `users`
@@ -236,7 +237,8 @@ CREATE TABLE IF NOT EXISTS `users` (
 INSERT INTO `users` (`id`, `login`, `firstname`, `lastname`, `email`, `password`, `register_date`, `birth_date`, `phone`, `sex`, `admin`, `status`) VALUES
 (4, 'test', 'test', 'test', 'test@test.fr', '$2y$08$0qBo6z48Ljz2/VAXVvQvQe106MYCZP6hi0KguP2pnAqjQmkAYz/pm', '2016-06-02 11:43:19', '1990-11-29', '0688330599', 'W', 0, 1),
 (5, 'mickwan', 'mickael', 'rinner', 'mickael.rinner@hotmail.fr', '$2y$08$Kt2LF.bt0fnebG3PbOdyaeMFezNkKkH6NYlYEOZezi87OWMShme3m', '2016-06-02 11:44:14', '1990-11-29', '0688330599', 'M', 1, 1),
-(6, 'Arteast', 'thomas', 'loegel', 'arteast.academy@gmail.com', '$2y$08$ZtSu9f6l2rSZ8RAmxvLlmOxB9.o914eAeO89QOBb78lX9qoC4i8Qe', '2016-06-06 12:51:35', '1986-11-02', '0771719925', 'M', 1, 1);
+(6, 'Arteast', 'thomas', 'loegel', 'arteast.academy@gmail.com', '$2y$08$ZtSu9f6l2rSZ8RAmxvLlmOxB9.o914eAeO89QOBb78lX9qoC4i8Qe', '2016-06-06 12:51:35', '1986-11-02', '0771719925', 'M', 0, 1),
+(7, 'momo', 'momo', 'momo', 'momo@momo.fr', '$2y$08$KE/UywIyGnAhPfcIu2etZOowXy2CuEis1OR0vmvLzXR.SwlaRO/1u', '2016-06-08 13:30:26', '1990-11-29', '0688330599', 'M', 0, 1);
 
 --
 -- Contraintes pour les tables exportées
